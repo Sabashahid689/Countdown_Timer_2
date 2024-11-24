ae470d9cd5c7b3a175912293daf28e5df535d0b3
Sabashahid689 <nabegashahid@gmail.com>
2024-11-24 15:30:12 +0500

    first commit
    countdown timer

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Countdown from "@/components/count_down";
+
+export default function home (){
+  return (
+       <div className="min-h-screen flex items-center justify-center bg-gray-900">     
+        <Countdown />
+    </div>
+  );
+}
+
